e768cb949140821bb0cb452ad1f1c2496393c95d
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-16 19:05:23 -0700

    WIP. add button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,25 +1,10 @@
-import { styled } from '@stitches/react';
+import { classNames } from '@/utils/classnames';
 import { ButtonHTMLAttributes } from 'react';
 
-const ToggleButton = styled('button', {
-    padding: '1.2rem .5rem',
-    left: 'calc(50vw - 4.5rem)',
-    width: '9rem',
-    height: '2rem',
-
-    border: '.5px solid grey',
-    borderRadius: '3px',
-    color: 'white',
-    backgroundColor: '#FF7518',
-    cursor: 'pointer',
-
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-});
-
-export function StartButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export function StartButton({ children, className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
-        <ToggleButton {...props} />
+        <button className={classNames("px-4 py-3 bg-orange-400 border-gray-400 border rounded-md shadow", className)} {...rest}>
+            {children}
+        </button>
     );
 }
