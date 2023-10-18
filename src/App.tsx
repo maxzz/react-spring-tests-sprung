c501278b62fb7dbf45361e14360d426f952a3bb4
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-17 20:05:37 -0700

    WIP. add custom scrollbars

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { PageUseChain } from './components/animations/5-useChain';
 
 export function App() { // Uncomment to see examples
     return (
-        <div className="mx-auto max-w-sm md:max-w-5xl bg-gray-500">
-            <PageUseSpring />
-            <PageUseSprings />
-            <PageUseTransition />
-            <PageUseTrail />
-            <PageUseChain />
+        <div className="h-screen overflow-overlay smallscroll">
+            <div className="mx-auto max-w-sm md:max-w-5xl bg-gray-500">
+                <PageUseSpring />
+                <PageUseSprings />
+                <PageUseTransition />
+                <PageUseTrail />
+                <PageUseChain />
+            </div>
         </div>
     );
 }
