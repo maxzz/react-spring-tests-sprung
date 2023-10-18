9674c781e835db9ddbea50a97fd901e34302b04d
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-17 20:10:15 -0700

    WIP. update all start buttons

--- src/components/ui/controls/button.tsx
+++ src/components/ui/controls/button.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from 'react';
 
 export function StartButton({ children, className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
-        <button className={classNames("px-4 py-3 text-sm bg-orange-400 border-gray-400 border rounded-md shadow select-none", className)} {...rest}>
+        <button className={classNames("px-4 py-3 text-sm bg-gray-600 border-gray-400 border rounded-md shadow shadow-gray-100/30 select-none active:scale-y-[.97]", className)} {...rest}>
             {children}
         </button>
     );
